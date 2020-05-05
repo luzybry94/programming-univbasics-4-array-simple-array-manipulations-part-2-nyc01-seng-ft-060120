52f24aaf1f0c10697fddf18094f53bf4f400f1e0
Chaim Bryski <luzybry94@gmail.com>
2020-05-05 22:50:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,4 +7,13 @@ def using_insert(array)
 end
 
 def using_flatten(array)
-  
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, int)
+  array.delete_at(int)
+end
